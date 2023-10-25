5d0506d2b2ebe0e6a1c93b607356d66890b3cc41
Smith <rdsmith@wm.edu>
2023-10-25 14:52:34 -0400

    Update main_store.py

--- main_store.py
+++ main_store.py
@@ -1,20 +1,4 @@
-# Example to include
-
-# A database to store and retrieve data
-
-# [_] When switching how to display customers, retain current selection
-
-# An interface with:
-#   plot
-#   tabular data
-#      download as csv
-#   Buttons
-#   Slider bars
-#
-#   Something in a sidebar
-#
 # Do a version 1 and 2 that switches styles (e.g., crowded dashboard vs minimal)
-
 import src.my_app as app
 
 if __name__ == '__main__':
